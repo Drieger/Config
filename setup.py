7a58f771741187eb55807d3c1acf2d63527e85d6
Daniel Beckert <drbeckert@gmail.com>
2019-01-29 14:22:57 -0200

    Start theme and icon setup

--- setup.py
+++ setup.py
@@ -13,18 +13,21 @@
     * plug package manager
     * nvm
     * arc-theme
+    * radix icons
     * wallpaper
 
     :created by Drieger
 """
 import os
+import shutil
+import pathlib
 
 
 CHECK_MARK = "\u2713"
 BALLOT_MARK = "\u2717"
 LEFT_PADDING = "  "
 
-def set_wallpaper(location=None):
+def setup_wallpaper(location=None):
     """
     Change the wallpaper to the one in the provided uri
     """
@@ -40,6 +43,25 @@ def set_wallpaper(location=None):
         os.system("gsettings set org.gnome.desktop.screensaver picture-uri {}".format(location))
         os.system("gsettings set org.gnome.desktop.background picture-uri {}".format(location))
         print(output.format(padding=LEFT_PADDING, mark=CHECK_MARK))
+
+def setup_theme():
+    """
+    Install and change ubuntu theme to arc-theme
+    """
+    output = "{padding}{mark} Changing theme..."
+    os.system('sudo apt install arc-theme')
+    print(output.format(padding=LEFT_PADDING, mark=BALLOT_MARK))
+
+def setup_icons():
+    """
+    Install icons package
+    """
+    os.chdir('/tmp')
+    shutil.rmtree('flat-remix', ignore_errors=True)
+    os.system('git clone https://github.com/daniruiz/flat-remix')
+    shutil.copytree('/tmp/flat-remix', os.path.join(str(pathlib.Path.home()), '.icons'))
+    os.system('gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix"')
+
 
 def setup_vim():
     """
@@ -75,9 +97,10 @@ sudo update-alternatives --set vi /usr/local/bin/vim
     """
     pass
 
-:se
 
 if __name__ == "__main__":
     print("Welcome, we will help you to setup your new environment\n")
-    set_wallpaper()
+    #  setup_wallpaper()
+    #  setup_theme()
+    setup_icons()
 
